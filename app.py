fb83b8f09fa209b20e1534ee19db25e5e17b72d6
Tami Gartner <31107704+tagaertner@users.noreply.github.com>
2023-08-30 10:44:19 -0400

    setup of banking assignment

---
+++ app.py
@@ -0,0 +1,12 @@
+
+
+def atm_menu(name):
+    print("")
+    print("          === Automated Teller Machine ===          ")
+    print("User: " + name)
+    print("------------------------------------------")
+    print("| 1.    Balance     | 2.    Deposit      |")
+    print("------------------------------------------")
+    print("------------------------------------------")
+    print("| 3.    Withdraw    | 4.    Logout       |")
+    print("------------------------------------------")
