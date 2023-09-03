6406a566976e7e79633787a9e2a17dd9cab24918
Tami Gartner <31107704+tagaertner@users.noreply.github.com>
2023-09-03 15:05:57 -0400

    fixed bugs and added security of max attemps in the login section

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-"""Week 2 workshop Assigment, baking taks
+"""Week 2 Workshop Assigment, banking task
 by Tami Gaertner
-last edited on 09/1/23"""
-# import from banking pkg
+last edited on 09/2/23"""
+# import from banking pkg and import locale is to show money in USD currency
 from banking_pkg.account import show_balance, deposit, withdraw, logout
 import locale
 
-COMPANY_NAME = "Company"
-def atm_menu(name,balance,deposit, withdraw, logout):
+def atm_menu(name, balance):
     print("")
     print("          === Automated Teller Machine ===          ")
     print("User: " + name)
@@ -17,47 +16,68 @@ def atm_menu(name,balance,deposit, withdraw, logout):
     print("| 3.    Withdraw    | 4.    Logout       |")
     print("------------------------------------------")
 
-#taks 2 Registration of user and pin set up
-#bonus 1 names 1-10 character are valid, if invalid show error message and promt to do it again
-#bonus 2 pin can only be 4 characters, send error message if more or less than 4 and to try again
+#Task 2 :Registration of user and pin set up
+#Bonus 1: names 1-10 character are valid, if invalid show error message and promt to do it again
+#Bonus 2: pin can only be 4 characters, send error message if more or less than 4 and to try again
 def create_account():
     user = ""
     pin = ""
     balance = 0
+  
     while True:
         print("          === Automated Teller Machine ===          ")
         user = input("Enter a name to register:\n")
 
         if len(user) >= 1 and len(user) <= 10:
-            pin = input("Enter 4-digit PIN:\n")
-            if pin.isdigit() and len(pin) == 4:
-                print(f"{user} has been registered with a starting balance of ${balance}\n")
-                break
-            else:
-                print("Invalid PIN entered. PIN must be 4 digits.")
+               break
         else:
             print("Invalid name. Name must be between 1 and 10 characters.")
+                
+    while True:            
+            pin = input("Enter 4-digit PIN:\n") 
+            if pin.isdigit() and len(pin) == 4:
+                    print(f"{user} has been registered with a starting balance of ${balance}\n")
+                    break
+               
+            else:
+                    print("Invalid PIN entered. PIN must be 4 digits.")
+          
     return (user, pin, balance)
 
-#task 3 while loop that request the users name and pin 
-#
+#Task 3: Login request the users name and pin 
 def login(name, pin):
-    while True:
+    #for security max attempts of 3, then will exit code if unsuccessful 
+    max_attempts = 3 
+    attempts = 0
+    login_successful = False
+    
+    while attempts < max_attempts:
         print("          === Automated Teller Machine ===          ")
+        print("LOGIN")
         name_to_validate = input("Enter name: ")
-        pin_to_validate = input("Enter PIN: ")
 
-        if name_to_validate == name and pin_to_validate == pin:
-            print("Login successful!")
-            break
-        if name_to_validate != name or pin_to_validate != pin:
+        if name_to_validate == name:
+            pin_to_validate = input("Enter PIN: ")
+            if pin_to_validate == pin:
+                print("Login successful!")
+                login_successful = True
+                break
+            else:
+                print("Invalid credentials!")
+                attempts += 1
+                break
+        else:
             print("Invalid credentials!")
+            attempts += 1
+    if attempts == max_attempts:
+        print("Maximum login attempts reached. Please try again later.\n")
+        
+    return login_successful
 
-
-#taks 5 add the 4 options from the account module
-def option_select(name, balance, deposit, withdraw, logout):
+#taks 5 creat the 4 options link the account module to this page
+def option_select(name, balance):
     while True:
-        atm_menu(name, balance, deposit, withdraw, logout)
+        atm_menu(name, balance)
         option = input(f"{name}, choose an option: ")
         print("You chose option:", option)
 
@@ -73,10 +93,17 @@ def option_select(name, balance, deposit, withdraw, logout):
 
     return balance
 
+#to make sure that this page runs first, per best practices
 if __name__ == "__main__":
     
     balance = 0
     name ="" 
     (name, pin, balance) = create_account()
-    login(name, pin)
-    balance = option_select(name, balance, deposit, withdraw, logout)
+    
+    #this code is executed after the login block of code runs. 
+    #checks the retun vaule, if True the option_select will run
+    #if returns False the else block of code will execute and exit.  
+    if login(name, pin):
+         balance = option_select(name, balance)
+    else:
+        exit(0)
--- banking_pkg/account.py
+++ banking_pkg/account.py
@@ -1,8 +1,9 @@
-#taks 4 write 4 functions
+#Task 4: write 4 options that will run on app.py
+# import locale is to covert to USD currency
 import locale
-# function 1 show_balance
-# code should display the account balance of the user. use float to convert from str
 
+# function 1 show_balance
+# code should display the account balance of the user. 
 
 def show_balance(balance):
     if balance ==0:
@@ -11,8 +12,8 @@ def show_balance(balance):
         locale.setlocale(locale.LC_ALL, '')
         usd_balance = locale.currency(balance, grouping = True)
         print(f"Current balance:{usd_balance}\n")
+    
     return balance
-
 
 #function2 deposite. User inputs amount depostied and the new balance will be printed
 def deposit(balance):
@@ -24,8 +25,8 @@ def deposit(balance):
     
     return balance
 
-
 #function 3 withdraw user inputs amount withdraw and new balance will print out
+# use cannot overdraft, will create an error message
 def withdraw(balance):   
     locale.setlocale(locale.LC_ALL, '')
     amount = float(input("Enter amount to withdraw:"))
@@ -36,15 +37,12 @@ def withdraw(balance):
     else:
         print(" Insufficent funds\n")
     
-    return balance
-
-    
+    return balance 
 
 #function 4 logout
 def logout(name):
-    print(f"goodbye {name}\n")
+    print(f"Goodbye {name}\n")
 
 
-\
 
   
